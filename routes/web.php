798e10d13aed5d43dea11c8db67166d226f2981c
kaetaen <kaetaen@tutanota.com>
2021-02-22 00:49:25 -0300

    Understanding the usage of where keywords

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,36 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
 
     /*
-     *  Translating raw sql query using query builder
+     * Sintaxes válidas para multiplas condicionais usando where
      */
 
     $actors = DB::table('actor')->where('last_name', '=', 'Berry')->get();
 
+    $actors = DB::table('actor')->where([
+        ['last_name', 'Berry'],
+        ['first_name', 'Karl']
+    ]);
+
+    $actors = DB::table('actor')
+        ->where(function ($query) {
+            $query->where('last_name', 'Berry');
+        })->get();
+
+    /*
+     *  SELECT last_name, count(*) AS actor_count
+     *  from actor
+     *  group by last_name
+     *  order by actor_count desc
+     *
+     * */
+
+    $actors = DB::table('actor')
+        ->select(['last_name', DB::raw('count(*) as actor_count')])
+        ->groupBy('last_name')
+        ->orderBy('actor_count', 'desc')
+        ->get();
+
+
     return $actors;
+    
 });
